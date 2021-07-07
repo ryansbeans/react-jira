6ca64dada424a92adeb21ba06ea412691af034fc
v_junfupeng <522296700@qq.com>
2021-07-07 15:56:19 +0800

    chore: 添加ts类型定义

---
+++ src/utils/index.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
+// 在一个函数里,改变传入的对象本身是不好的
+
+interface ObjectProps {
+  object: {};
+}
+export const cleanObject = (object: ObjectProps) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value: any, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    // 每当value变化后,设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完后再执行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
